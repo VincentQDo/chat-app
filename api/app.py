53d223227f27eeddab447daf809e22eb8c2de127
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-04-08 21:57:30 -0400

    websocket server working

--- api/app.py
+++ api/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+import asyncio
+import websockets
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<p>Hello World</p>"
+async def hello(websocket):
+    while True:
+        name = await websocket.recv()
+        print(f"<<< {name}")
+
+        greeting = f"Hello {name}!"
+
+        await websocket.send(greeting)
+        print(f">>> {greeting}")
+
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
