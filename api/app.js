8e2ac44ede9bb74cc4bd9f9902d223ac1d8827ff
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-04-13 09:59:04 -0400

    change to nodejs server instead of python

---
+++ api/app.js
@@ -0,0 +1,22 @@
+import WebSocket, { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({
+  port: 8080,
+  clientTracking: true,
+  path: '/chat',
+})
+
+wss.on('connection', (wsClient, req) => {
+  const connectedMsg = `${req.socket.remoteAddress} connected`
+  console.log(connectedMsg)
+  wss.clients.forEach(e => e.send(connectedMsg))
+  wsClient.on('message', (data) => {
+    const strData = data.toString()
+    console.log(`<<< ${strData}`)
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(strData);
+      }
+    })
+  })
+})
