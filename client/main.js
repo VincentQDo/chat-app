5877ed9ec232ced3613c7df3e36f979388c479db
Vincent Q Do <38704970+VincentQDo@users.noreply.github.com>
2024-04-17 21:52:32 -0400

    add connect button and username

--- client/main.js
+++ client/main.js
@@ -1,21 +1,53 @@
 import "./style.css";
 import { sendMessage, setupConnection } from "./sendMessage.js";
 
-document.querySelector("#app").innerHTML = `
+function app(userName) {
+  return `
   <div>
+    <h3>Welcome ${userName}</h3>
     <div id="messageBox"></div>
     <input id="userInput"/>
     <button id="send" type="button" disabled>Send</button>
-  </div>
-`;
+  </div>`;
 
-setupConnection(() => {
-  const buttonEle = document.getElementById("send");
-  buttonEle.disabled = false;
-  buttonEle.addEventListener("click", () => {
-    sendMessage(
-      document.getElementById("userInput"),
-      document.getElementById("messageBox")
-    );
-  });
-});
+}
+
+function login() {
+  return `
+  <div>
+    <label for="userNameInput">Username:</label>
+    <input name="userNameInput" id="userNameInput"/>
+    <button id="connect" type="button">Connect</button>
+  </div>
+  `
+}
+
+function updateAppState(userName) {
+  document.querySelector("#app").innerHTML = userName ? app(userName) : login()
+  if (userName) {
+    setupConnection(() => {
+      const buttonEle = document.getElementById("send");
+      buttonEle.disabled = false;
+      buttonEle.addEventListener("click", () => {
+        sendMessage(
+          document.getElementById("userInput"),
+          document.getElementById("messageBox")
+        );
+      });
+    });
+
+  }
+}
+
+updateAppState('')
+
+const connecBtn = document.getElementById('connect')
+connecBtn.addEventListener('click', () => {
+  /**
+   * @type {HTMLInputElement}
+   */
+  const userNameInput = document.getElementById('userNameInput')
+
+  updateAppState(userNameInput.value ? userNameInput.value : '')
+})
+
