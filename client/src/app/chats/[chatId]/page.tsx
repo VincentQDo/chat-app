b7301c17bde0b581a8c2492a9426ad409152862a
Vince <vincentquangdo@gmail.com>
2024-06-28 13:32:08 -0400

    Successfully fetch initla data from server

--- client/src/app/chats/[chatId]/page.tsx
+++ client/src/app/chats/[chatId]/page.tsx
@@ -1,77 +1,20 @@
-"use client";
-
-import Message from "@/components/Message";
+import ChatInterface from "@/components/ChatInterface";
 import { ChatMsg } from "@/models/ChatMsg";
-import { fetchData } from "@/utils/ServerActions";
-import Link from "next/link";
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 
-export default function Chat({ params }: { params: { chatId: string } }) {
-
-  const [messageList, setMessageList] = useState([] as ChatMsg[]);
-  const [userInput, setUserInput] = useState('');
-  const socketRef = useRef<WebSocket | null>(null);
-
-  useEffect(() => {
-    fetchData((messageList: ChatMsg[]) => {
-      // TODO can't define the call back function in a client component like this because only server actions can be passed as call back to server actions
-      setMessageList(messageList);
-    }, params.chatId);
-  }, [params.chatId]);
-
-  useEffect(() => {
-    socketRef.current = new WebSocket('ws://localhost:8080/chat');
-
-    socketRef.current.onopen = () => {
-      console.log('Socket connection opened');
+export default async function Chat({ params }: { params: { chatId: string } }) {
+  try {
+    const res = await fetch(`http://localhost:8080/messagelist?chatid=${params.chatId}`);
+    if (!res.ok) {
+      throw new Error(`HTTP error ${res.status}: ${res.statusText}`)
     }
-
-    socketRef.current.onmessage = (event: MessageEvent<{ message: string }>) => {
-      const newMessage = event.data;
-      console.log(newMessage);
-    }
-
-    socketRef.current.onclose = () => {
-      console.log('websocket connectio nclosed')
-    }
-
-    return () => {
-      socketRef.current?.close();
-    }
-  }, []);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const newChatMsg: ChatMsg = {
-      message: userInput,
-      userId: 'vince1',
-      role: 'self',
-      userName: 'Vince',
-      messageId: 'fjlkwe'
-    };
-    socketRef.current?.send(userInput);
-    setMessageList([...messageList, newChatMsg]);
-  };
-
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value)
-    console.log(userInput)
+    const messageList: ChatMsg[] = await res.json();
+    return (
+      <ChatInterface initialData={messageList}></ChatInterface>
+    )
+  } catch (error) {
+    console.error('Error while getting messages', error);
+    return (
+      <ChatInterface initialData={[]}></ChatInterface>
+    )
   }
-
-  return (
-    <div>
-      <Link href="/chats">Back to convo List</Link>
-      {messageList.map((e) => (
-        <Message message={e} key={e.messageId}></Message>
-      ))}
-
-      <form method="post" onSubmit={handleSubmit}>
-        <label>
-          Chat Message:
-          <input name="chatMsg" value={userInput} onChange={onChangeHandler} className="bg-slate-800"></input>
-        </label>
-        <button role="submit">Submit</button>
-      </form>
-    </div>
-  );
 }
---
+++ client/src/components/ChatInterface.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import Message from "@/components/Message";
+import { ChatMsg } from "@/models/ChatMsg";
+import Link from "next/link";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
+
+export default function ChatInterface({ initialData }: { initialData: ChatMsg[] }) {
+
+    const [messageList, setMessageList] = useState(initialData);
+    const [userInput, setUserInput] = useState('');
+    const socketRef = useRef<WebSocket | null>(null);
+
+    useEffect(() => {
+        socketRef.current = new WebSocket('ws://localhost:8080/chat');
+
+        socketRef.current.onopen = () => {
+            console.log('Socket connection opened');
+        }
+
+        socketRef.current.onmessage = (event: MessageEvent<{ message: string }>) => {
+            const newMessage = event.data;
+            console.log(newMessage);
+        }
+
+        socketRef.current.onclose = () => {
+            console.log('websocket connectio nclosed')
+        }
+
+        return () => {
+            socketRef.current?.close();
+        }
+    }, []);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const newChatMsg: ChatMsg = {
+            message: userInput,
+            userId: 'vince1',
+            role: 'self',
+            userName: 'Vince',
+            messageId: 'fjlkwe'
+        };
+        socketRef.current?.send(userInput);
+        setMessageList([...messageList, newChatMsg]);
+    };
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setUserInput(e.target.value)
+        console.log(userInput)
+    }
+
+    return (
+        <div>
+            <Link href="/chats">Back to convo List</Link>
+            {messageList.map((e) => (
+                <Message message={e} key={e.messageId}></Message>
+            ))}
+
+            <form method="post" onSubmit={handleSubmit}>
+                <label>
+                    Chat Message:
+                    <input name="chatMsg" value={userInput} onChange={onChangeHandler} className="bg-slate-800"></input>
+                </label>
+                <button role="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
