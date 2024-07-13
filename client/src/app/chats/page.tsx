c522edc4b2dc2dc678fd6328af79981c3871a283
MSI laptop <vincentquangdo@gmail.com>
2024-07-13 02:03:45 +0000

    add redirect when unauth

--- client/src/app/chats/page.tsx
+++ client/src/app/chats/page.tsx
@@ -1,14 +1,27 @@
 import { ChatList } from "@/models/ChatList";
 import Link from "next/link";
+import { permanentRedirect, redirect } from "next/navigation";
 
 export default async function Chats() {
     let chatList: ChatList[] = [];
+    let isUnauthorized: boolean = false;
     try {
         const userid = '01';
         const res = await fetch(`http://localhost:8080/chatlist?userid=${userid}`);
-        chatList = await res.json();
+        res.bodyUsed
+        const jsonRes = await res.json();
+        console.log(jsonRes);
+        if (Array.isArray(jsonRes)) {
+            chatList = jsonRes;
+        } else {
+            isUnauthorized = true;
+        }
     } catch (error) {
-        console.error(error)
+        console.error(error);
+        isUnauthorized = true;
+    }
+    if (isUnauthorized) {
+        redirect('/unauthorized');
     }
     // TODO should get data from some api call
     return (
