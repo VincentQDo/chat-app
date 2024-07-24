097723a2c9060e66d453136390ce833d35945acd
vincentquangdo@gmail.com <vincentquangdo@gmail.com>
2024-07-24 00:32:55 +0000

    update db to streamline it

--- api/database/chatlist.sql
+++ api/database/chatlist.sql
@@ -1,12 +1,4 @@
-CREATE TABLE IF NOT EXISTS chats (
-	sessionId INTEGER PRIMARY KEY AUTOINCREMENT,
-	createdAt INTEGER,
-	updatedAt INTEGER,
-	isGroupChat BOOLEAN
-);
-
 CREATE TABLE IF NOT EXISTS users (
-
 	userId TEXT PRIMARY KEY,
 	userName TEXT
 );
@@ -21,14 +13,10 @@ CREATE TABLE IF NOT EXISTS user_chats (
 
 CREATE TABLE IF NOT EXISTS messages (
 	messageid INTEGER PRIMARY KEY AUTOINCREMENT,
-	chatId INTEGER,
 	userId TEXT,
-	userName TEXT,
 	message TEXT,
-	role TEXT,
 	timestamp INTEGER,
 	status TEXT DEFAULT 'sent',
-	FOREIGN KEY (chatId) REFERENCES chats(sessionId) ON DELETE CASCADE,
 	FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
 );
 
@@ -36,13 +24,12 @@ CREATE TABLE IF NOT EXISTS friends (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	userId TEXT,
 	friendId TEXT,
-	status TEXT,
+	status TEXT DEFAULT 'pending',
 	createdAt INTEGER,
 	updatedAt INTEGER,
 	FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
 	FOREIGN KEY (friendId) REFERENCES users(userId) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_chatId ON messages(chatId);
 CREATE INDEX IF NOT EXISTS idx_userId ON messages(userId);
 CREATE INDEX IF NOT EXISTS idx_timestamp ON messages(timestamp);
