e8d49b5b0e3364d0b73a004d00b117c3e9d4bcc8
vincentquangdo@gmail.com <vincentquangdo@gmail.com>
2024-08-09 01:51:16 +0000

    add missing semi colon

--- client/src/app/globalchat/page.tsx
+++ client/src/app/globalchat/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { useEffect, useRef, useState } from 'react';
-import { io, Socket } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client';
+
 export default function GlobalChat() {
   const [userInput, setUserInput] = useState('');
 
   const socket = useRef<Socket | null>(null);
+
   useEffect(() => {
     socket.current = io('http://localhost:8080');
     socket.current.on('connect', () => {
@@ -23,6 +25,7 @@ export default function GlobalChat() {
       message: userInput
     })
   }
+
   return <div>
     <input type="text" className="bg-slate-700" value={userInput} onChange={(event) => setUserInput(event.target.value)}></input>
     <button onClick={() => sendMessage()}>Send</button>
