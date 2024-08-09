caf699ee238db21c70d6c02eeb123852ff0dea0a
vincentquangdo@gmail.com <vincentquangdo@gmail.com>
2024-08-09 01:48:35 +0000

    can now send message to backend via websocket

--- client/src/app/globalchat/page.tsx
+++ client/src/app/globalchat/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEffect, useRef, useState } from 'react';
+import { io, Socket } from 'socket.io-client'
 export default function GlobalChat() {
-  return <div>GlobalChat</div>
+  const [userInput, setUserInput] = useState('');
+
+  const socket = useRef<Socket | null>(null);
+  useEffect(() => {
+    socket.current = io('http://localhost:8080');
+    socket.current.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+    })
+
+    return () => {
+      socket.current?.disconnect()
+    }
+  }, [])
+
+  const sendMessage = () => {
+    socket.current?.emit('message', {
+      userid: 'userTest',
+      message: userInput
+    })
+  }
+  return <div>
+    <input type="text" className="bg-slate-700" value={userInput} onChange={(event) => setUserInput(event.target.value)}></input>
+    <button onClick={() => sendMessage()}>Send</button>
+  </div>
 }
