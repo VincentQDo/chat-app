2ea6c7569c41bfc64b34f577a561d9158d671ae6
vincentquangdo@gmail.com <vincentquangdo@gmail.com>
2024-08-10 01:37:41 +0000

    Add chat display

--- client/src/app/globalchat/page.tsx
+++ client/src/app/globalchat/page.tsx
@@ -5,11 +5,13 @@ import { io, Socket } from 'socket.io-client';
 
 export default function GlobalChat() {
   const [userInput, setUserInput] = useState('');
+  const [messages, setMessages] = useState<string[]>([]);
 
   const socket = useRef<Socket | null>(null);
 
   useEffect(() => {
     socket.current = io('http://localhost:8080');
+    console.log('creating new socket connection')
     socket.current.on('connect', () => {
       console.log('Connected to Socket.IO server');
     })
@@ -20,6 +22,7 @@ export default function GlobalChat() {
   }, [])
 
   const sendMessage = () => {
+    setMessages([...messages, userInput])
     socket.current?.emit('message', {
       userid: 'userTest',
       message: userInput
@@ -27,7 +30,12 @@ export default function GlobalChat() {
   }
 
   return <div>
-    <input type="text" className="bg-slate-700" value={userInput} onChange={(event) => setUserInput(event.target.value)}></input>
+    <ul>
+      {messages.map((message, index) => <li key={index}>{message}</li>)}
+    </ul>
+    <input
+      type="text"
+      className="bg-slate-700" value={userInput} onChange={(event) => setUserInput(event.target.value)}></input>
     <button onClick={() => sendMessage()}>Send</button>
   </div>
 }
