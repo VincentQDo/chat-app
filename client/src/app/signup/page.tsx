47c332dc82e39763ed284f27c6b96c8158c031f0
vincentquangdo@gmail.com <vincentquangdo@gmail.com>
2024-08-13 02:00:21 +0000

    Add signin and signup forms

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -1,20 +1,71 @@
-'use client';
+'use client'
 
-import { signUp } from '@/services/firebase';
+import { signUp } from '@/services/firebase'
+
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export default function SignUp() {
     const submitHandler = async (formData: FormData) => {
-        const [email, password] = [formData.get('email')?.toString(), formData.get('password')?.toString()];
+        const [email, password] = [formData.get('email')?.toString(), formData.get('password')?.toString()]
         if (email && password) {
-            const user = await signUp(email, password);
-            console.log(user);
+            const user = await signUp(email, password)
+            console.log(user)
         }
     }
 
     return (
-        <form action={submitHandler}>
-            <input className="bg-black" type="email" name="email" placeholder="Email" required />
-            <input className='bg-black' type="password" name="password" placeholder="Password" required />
-            <button type="submit">Create Account</button>
-        </form>
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+            <div className="flex items-center justify-center py-12">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-3xl font-bold">Sign up</h1>
+                        <p className="text-balance text-muted-foreground">
+                            Enter your information to create an account
+                        </p>
+                    </div>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="username">Username</Label>
+                            <Input
+                                id="username"
+                                type="text"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Create an account
+                        </Button>
+                        <Button variant="outline" className="w-full">
+                            Signup with Google
+                        </Button>
+                    </div>
+                    <div className="mt-4 text-center text-sm">
+                        Already have an account?{" "}
+                        <Link href="../signin" className="underline">
+                            Sign in
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
