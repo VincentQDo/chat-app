92f6dfadf92c9fbea64cce3cc8107ebff4f3a14d
VinceQDo <vincentquangdo@gmail.com>
2025-03-28 11:19:39 +0000

    add development config for serviceaccount

--- api/src/utilities/firebaseAdmin.js
+++ api/src/utilities/firebaseAdmin.js
@@ -1,9 +1,16 @@
 // firebaseAdmin.js
 import admin from 'firebase-admin';
 
-console.log('[INFO] service account env: ', process.env.FIREBASE_SERVICEACCOUNT)
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICEACCOUNT)
-console.log('[INFO] service account: ', serviceAccount)
+console.log('[DEBUG] Node environment ', process.env.NODE_ENV)
+let serviceAccount
+if (process.env.NODE_ENV === 'development') {
+    serviceAccount = (await import('/workspace/service-account.json', { assert: { type: 'json' } })).default
+    console.log('[INFO] service account file: ', serviceAccount)
+} else {
+    console.log('[INFO] service account env: ', process.env.FIREBASE_SERVICEACCOUNT)
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICEACCOUNT)
+    console.log('[INFO] service account: ', serviceAccount)
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
