5c967e14bfc2eea13bb8a03087017a9ec9192352
VincentQDo <vincentquangdo@gmail.com>
2025-09-24 08:05:40 +0000

    feat: Refactor message editing and status handling to streamline updates and improve validation

--- database/routes/messages.js
+++ database/routes/messages.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addMessage, deleteMessage, editMessage, getAllMessages, insertOrUpdateMessageStatus } from "../db/database.js";
+import { addMessage, deleteMessage, editMessage, getAllMessages, markMessagesAsReadPrepared } from "../db/database.js";
 
 const router = express.Router();
 
@@ -16,9 +16,10 @@ router.post("/", async (req, res) => {
   res.json(data);
 });
 
-router.patch("/", async (req, res) => {
-  console.log("[INFO] Editing message from body", req.body);
-  const { messageId, newContent, status } = req.body;
+router.patch("/:messageId/content", async (req, res) => {
+  console.log("[INFO] Editing message from body", req.params, req.body);
+  const { messageId  } = req.params;
+  const { newContent  } = req.body;
   if (!messageId) {
     res.status(400).json({ error: "messageId is required" });
     return;
@@ -27,17 +28,26 @@ router.patch("/", async (req, res) => {
   if (typeof newContent === "string") {
     const data = await editMessage(messageId, newContent);
     res.json(data);
-  } else if (status) {
-    const userId = req.body.userId;
-    if (!userId) {
-      res.status(400).json({ error: "userId is required to set status" });
-      return;
-    }
-    const data = await insertOrUpdateMessageStatus(messageId, userId, status);
-    res.json(data);
   } else {
     res.status(400).json({ error: "nothing to update" });
   }
+});
+
+router.patch("/status", async (req, res) => {
+  console.log("[INFO] Editing message from body", req.body);
+  const { statuses } = req.body;
+  if (!Array.isArray(statuses) || statuses.length === 0) {
+    res.status(400).json({ error: "Invalid request" });
+    return;
+  }
+  statuses.forEach(status => {
+    if (!status.messageId || !status.userId || !status.status) {
+      res.status(400).json({ error: "Invalid request" });
+      return;
+    }
+  });
+  const result = await markMessagesAsReadPrepared(statuses);
+  res.json(result);
 });
 
 router.delete("/", async (req, res) => {
