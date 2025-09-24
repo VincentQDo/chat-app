9c4cbaad47ec1ace3148f77bed60550e45eaf6c4
VincentQDo <vincentquangdo@gmail.com>
2025-09-24 08:00:47 +0000

    feat: Update Message type to use MessageStatus array for improved status tracking

--- database/types/message.d.js
+++ database/types/message.d.js
@@ -8,5 +8,13 @@
  * @property {number} createdAt - Timestamp when the message was created (in milliseconds since epoch)
  * @property {number|null} editedAt - Timestamp when the message was last edited (null if never edited)
  * @property {boolean} isDeleted - Flag indicating if the message has been deleted
- * @property {string} status - Status of the message ('sent', 'delivered', 'read')
+ * @property {MessageStatus[]} statuses - Status of the message ('sent', 'delivered', 'read')
+ */
+
+/**
+ * @typedef {Object} MessageStatus
+ * @property {string} messageId - Unique identifier for the message
+ * @property {string} userId - Identifier of the user associated with this status
+ * @property {string} status - Status of the message for this user ('sent', 'delivered', 'read')
+ * @property {number} updatedAt - Timestamp when the status was last updated (in milliseconds since epoch)
  */
